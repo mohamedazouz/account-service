cf1f94c639f2e7e153ad7d05e348d0b2f1127708
Mohamed Azouz <mohamedaliazouz@gmail.com>
2020-01-22 23:27:53 +0100

    fix building jar

--- src/main/java/com/azouz/accountservice/Application.java
+++ src/main/java/com/azouz/accountservice/Application.java
@@ -7,16 +7,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.jooby.Jooby;
 import io.jooby.json.JacksonModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Application extends Jooby {
 
-    private static final Logger log = LoggerFactory.getLogger(AccountController.class);
-
     {
         install(new JacksonModule(new ObjectMapper()));
-
         final Injector injector = Guice.createInjector(new BeansConfig());
         final AccountController accountController = injector.getInstance(AccountController.class);
         mvc(accountController);
