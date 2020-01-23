9d498a93b67b57047add1bcb991e8c447e073197
Mohamed Azouz <mohamedaliazouz@gmail.com>
2020-01-23 23:16:27 +0100

    remove uneeded methods

--- src/main/java/com/azouz/accountservice/exception/InsufficientAccountBalanceException.java
+++ src/main/java/com/azouz/accountservice/exception/InsufficientAccountBalanceException.java
@@ -2,21 +2,7 @@ package com.azouz.accountservice.exception;
 
 public class InsufficientAccountBalanceException extends RuntimeException {
 
-    private static final long serialVersionUID = -3932580886051016069L;
-
-    public InsufficientAccountBalanceException() {
-        super();
-    }
-
     public InsufficientAccountBalanceException(final String message) {
         super(message);
-    }
-
-    public InsufficientAccountBalanceException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
-    public InsufficientAccountBalanceException(final Throwable cause) {
-        super(cause);
     }
 }
